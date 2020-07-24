599872aa1944252ac304974050d08b88a04057b0
priceds <amsarvesh@live.com>
2020-07-24 19:23:29 +0530

    Create hello.java
    Main File Containing "Hello World " Program.

---
+++ hello.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+ public class Main 
+ {
+ 
+ public static void main(String args[])
+ {
+ 
+ System.out.println("Hello World !);
+ 
+ 
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ }
